0dec749afef6b568b272350114d40ad22ebc7904
Amir Monadi <99682150+amirmn8@users.noreply.github.com>
2022-11-27 20:59:23 +0330

    add breadCrumb component

---
+++ components/breadCrumb.vue
@@ -0,0 +1,10 @@
+<script setup>
+const props = defineProps(["page1", "page2"]);
+</script>
+<template>
+  <div class="flex justify-end my-10 mx-200px">
+    <span class="text-my-green">{{ page2 }}</span>
+    <span class="px-1">&lt;</span>
+    <span>{{ page1 }}</span>
+  </div>
+</template>
