658c9b3248157a80e6027f343f1427730fa6a71b
Amir Monadi <99682150+amirmn8@users.noreply.github.com>
2022-11-28 13:22:58 +0330

    complete login page and button

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <navbarMenu />
+  <navbarMenu displayLogin="h-0" displayMenu="hidden" />
   <WeblogHead />
   <blogContent />
 </template>
--- components/navbarMenu.vue
+++ components/navbarMenu.vue
@@ -1,3 +1,6 @@
+<script setup>
+const props = defineProps(["displayMenu", "displayLogin"]);
+</script>
 <template>
   <div
     class="h-6vw bg-white shadow-mine hidden xl:flex flex-row-reverse items-center justify-center">
@@ -6,7 +9,7 @@
       <div>
         <li class="liMenu">
           <!-- دکمه -->
-          <outlinebutton title="ورود" />
+          <outlinebutton @click="displayLogin = 'h-screen'" title="ورود" />
           <!-- دکمه -->
         </li>
       </div>
@@ -22,5 +25,60 @@
         </li>
       </div>
     </ul>
+  </div>
+  <div class="flex xl:hidden items-center justify-between p-4">
+    <Icons-menuIcon
+      @click="displayMenu = 'flex'"
+      class="w-12 h-12 stroke-my-green" />
+    <div
+      style="direction: rtl"
+      class="w-screen h-max z-20 fixed top-0 right-0 bg-white flex-col items-center"
+      :class="displayMenu">
+      <Icons-iconlogo />
+      <ul class="flex flex-col gap-4 mx-4 my-4 text-xl">
+        <li class="py-4 text-center">خانه</li>
+        <li class="py-4 text-center">محصولات</li>
+        <li class="py-4 text-center">خدمات</li>
+        <li class="activeMenu py-4 text-center">وبلاگ</li>
+        <li class="py-4 text-center">
+          <!-- دکمه -->
+          <outlinebutton
+            @click="(displayLogin = 'h-screen'), (displayMenu = 'hidden')"
+            title="ورود" />
+          <!-- دکمه -->
+        </li>
+      </ul>
+      <Icons-closeIcon
+        @click="displayMenu = 'hidden'"
+        class="fixed top-3 right-3 stroke-my-green" />
+    </div>
+  </div>
+  <div
+    class="relative z-10"
+    aria-labelledby="modal-title"
+    role="dialog"
+    :class="displayLogin"
+    aria-modal="true">
+    <div
+      :class="displayLogin"
+      class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+    <div :class="displayLogin" class="fixed inset-0 z-10 overflow-y-auto">
+      <div
+        class="flex min-h-full items-end justify-center text-center sm:items-center sm:p-0">
+        <div
+          class="relative transform overflow-hidden px-10 sm:px-30vw py-278px max-h-screen rounded-lg bg-white text-left shadow-xl transition-all">
+          <div class="bg-white">
+            <h2 class="text-center text-5xl mb-20">ورود به حساب کاربری</h2>
+            <inputUser class="mb-16" />
+            <inputPassword class="mb-12" />
+            <fillbutton class="w-full h-14" title="ورود" />
+          </div>
+          <Icons-closeIcon
+            @click="displayLogin = 'h-0'"
+            class="fixed top-8 left-1/2 z-20 stroke-my-green" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
